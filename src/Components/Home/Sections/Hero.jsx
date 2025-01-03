80be9c2c3a81fd42d2e6ae2b64f3931dbfe16673
muhaiminultasin <tasinmuhaiminul@gmail.com>
2025-01-03 16:40:39 +0600

    hero

--- src/Components/Home/Sections/Hero.jsx
+++ src/Components/Home/Sections/Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <section className='text-white text-center bg-[url("./barber-hero.jpg")] w-full h-[50vh] md:h-[90vh] bg-cover bg-no-repeat bg-center'>
+    <section className='text-white text-center bg-[url("barber-hero.jpg")] w-full h-[50vh] md:h-[90vh] bg-cover bg-no-repeat bg-center'>
         <h1 className=' text-2xl md:text-3xl lg:text-5xl lg:w-[60%] mx-auto pt-[100px] lg:pt-[200px] font-bold lg:leading-[65px]'>Multiple Manhattan Locations. One Perfect Cut</h1>
 
         <p className=' mt-5 text-[20px] font-[500]'>Experience affordable and unmatched craftsmanship at Manhattan's finest barbershop</p>
